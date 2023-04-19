85751a2de5ea9451c09e2565dec367199616e333
saurabhkulkarni12 <51773054+saurabhkulkarni12@users.noreply.github.com>
2023-04-19 14:30:43 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,12 +1,12 @@
 class Welcome{
-  public void userGreeting(){
-  System.out.println("Welcome to java programming");
+  public void uGreeting(){
+  System.out].println("Welcome to java programming");
   }
 }
 
 class HelloWorld{
   public static void main(String args[]){
   Welcome wel = new Welcome();
-  wel.userGreeting();
+  well userGreeting();
   }
 }
