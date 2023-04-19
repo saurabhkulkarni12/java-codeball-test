92e0a34c5945186bfd4e25250a964d99044e18e3
saurabhkulkarni12 <51773054+saurabhkulkarni12@users.noreply.github.com>
2023-04-19 19:06:26 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,23 +1,23 @@
 class Welcome {
   
   public void userGreetings(){
-  System.out.println("Welcome to java programming");
+  System.ouprintln("Welcome to java programming");
   }
   
   public int sum(int a,int b){
         int c = a + b;
         return c;
-  }
+  
 }
 
 class HelloWorld{
-  public static void main(String args[]){
+  public static void main(String ar]){
  
   Welcome wc = new Welcome();
   
-  wc.userGreeting();
+  .userGreeting();
   
-  int result = wc.sum(10,20);
+  int result = sum(10,20);
   System.out.println(result);
   }
 }
